f2c699f228eaaf6f0e5cc02f387ac57b5ed37c39
greatabel <myreceiver2for2github@gmail.com>
2021-03-08 09:48:50 +0800

    Sample

--- i1rm_jump_points.py
+++ i1rm_jump_points.py
@@ -4,6 +4,10 @@ import csv
 targe_filename = "HBYC_Line2_Kiln_2021_Jan_12.csv"
 
 path = "Data/" + targe_filename
+
+#实际处理 注释掉这行
+path = "DataSample/" + targe_filename
+
 newpath = "ProcessedData/" + targe_filename
 
 bufsize = 65536
