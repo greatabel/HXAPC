e9b94b0fe15d15dea79063b16e7a0e6b2dd67273
greatabel <myreceiver2for2github@gmail.com>
2021-03-16 22:10:47 +0800

    i3sort fix

--- i3sort.py
+++ i3sort.py
@@ -1,4 +1,12 @@
 # https://github.com/ShadenSmith/csvsorter
-from csvsorter import csvsort
+# from csvsorter import csvsort
 
-csvsort('HXProcessedData/HBYC_Line2_Kiln_2021_Jan_12_28.csv', [30])
+# csvsort('HXProcessedData/HBYC_Line2_Kiln_2021_Jan_12_28.csv', [30])
+
+import csv
+with open('HXProcessedData/HBYC_Line2_Kiln_2021_Jan_12_28.csv', newline='') as csvfile:
+	rdr = csv.reader(csvfile)
+	l = sorted(rdr, key=lambda x: x[30], reverse=True)
+	with open('HXProcessedData/output.csv', 'w') as csvout:
+	    wrtr = csv.writer(csvout)
+	    wrtr.writerows(l)
