c8b8ee041f854d2265768509a95de55cce964b7c
greatabel <myreceiver2for2github@gmail.com>
2021-03-16 18:33:59 +0800

    finished i2rm

--- i2rm_jump_points.py
+++ i2rm_jump_points.py
@@ -11,3 +11,113 @@
 所有的数值只要是数值类型 我就平均下
 
 '''
+import csv
+from datetime import datetime, timedelta
+
+
+targe_filename = "HBYC_Line2_Kiln_2021_Jan_12_28.csv"
+
+path = "HXData/" + targe_filename
+
+#实际处理 注释掉这行
+# path = "HXDataSample/" + targe_filename
+
+newpath = "HXProcessedData/" + targe_filename
+
+bufsize = 65536*6*5
+#实际处理 注释掉这行
+# bufsize = 5000
+
+#窑味料量
+# Feeding_index = 28
+
+# 本次只考虑使用头煤 HeadCoal_index
+HeadCoal_index = 29
+
+def process(lines):
+    time_need_removed = []
+    rows = []
+    for i in range(0, len(lines)):
+
+        items = lines[i].split(',')
+        if items[0] not in ('timestamp'):
+            d = datetime.strptime(items[0], "%Y/%m/%d %H:%M:%S")
+            for span in filter_spans:
+                if d >= span[0] and d <= span[1]:
+                    rows.append(lines[i])
+    return rows
+     
+
+
+
+
+time_points_of_changes = []
+filter_spans = []
+
+def scan_for_change(lines):
+    global time_change
+
+    for i in range(0, len(lines)):
+
+        # print("窑喂料量Kiln_Feed_SP=", lines[i][28], 'Kiln_Burner_Coal_SP头煤=',lines[i][29])
+        if i - 1 >= 0 and i + 1 < len(lines):
+            # result = handleline(lines[i - 1], lines[i], lines[i + 1], lines[i + 2])
+            items = lines[i].split(',')
+            items_prev = lines[i-1].split(',')
+            items_next = lines[i+1].split(',')
+            if  (items_prev[HeadCoal_index] not in  ('Kiln_Burner_Coal_SP', '头煤') and \
+                items_prev[HeadCoal_index] != items[HeadCoal_index] and  \
+                items_prev[HeadCoal_index] != items_next[HeadCoal_index]):
+                if items[0] not in time_points_of_changes:
+                    time_points_of_changes.append(items[0])
+                    print(items[0], ' changes!')
+
+def main():
+    global filter_spans
+
+    with open(path, encoding="UTF-8") as infile:
+        while True:
+            lines = infile.readlines(bufsize)
+
+            if not lines:
+                break
+            rows = scan_for_change(lines)
+    print(len(time_points_of_changes), time_points_of_changes[0:10])
+
+    
+    # 找出想要保留的时间区间列表
+    for i in range(0, len(time_points_of_changes)):
+        if i + 1 < len(time_points_of_changes):
+            d = datetime.strptime(time_points_of_changes[i], "%Y/%m/%d %H:%M:%S")
+            d_next = datetime.strptime(time_points_of_changes[i+1], "%Y/%m/%d %H:%M:%S")
+            # print(time_points_of_changes[i], time_points_of_changes[i+1])
+            span = d_next - d
+            print('#'*10, d, d_next)
+            print(span, '@'*5, d+(span/3), d+(span*2/3))
+            filter_spans.append((d+(span/3), d+(span*2/3)))
+
+    print('\n')
+    # print(filter_spans[0][0], filter_spans[0][1])
+    with open(path, encoding="UTF-8") as infile:
+        while True:
+            lines = infile.readlines(bufsize)
+            print('len(lines)=', len(lines))
+            if not lines:
+                break
+
+            rows = process(lines)
+            print('len(rows)=', len(rows))
+            # print('process len(rows)=', len(rows), rows, '-'*10, '\n')
+            # for r in rows:
+            #     print(r, '#'*10, '\n')
+            with open(newpath, "a", newline="") as csvfile:                            
+                writer = csv.writer(csvfile) 
+
+                # if write_count == 0:
+                #     writer.writerow(header)
+                # write_count += 1
+                for row in rows:
+                    writer.writerow(row.split(','))
+
+if __name__ == "__main__":
+    main()
